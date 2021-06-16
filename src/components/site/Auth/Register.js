7e557a0352a080c9af35eb25441778530d6d35a5
ThomasCGlover <thomascodyglover@gmail.com>
2021-06-16 09:11:13 -0400

    changed Navbar styling

--- src/components/site/Auth/Register.js
+++ src/components/site/Auth/Register.js
@@ -1,10 +1,30 @@
+import { render } from '@testing-library/react';
 import React, {useState} from 'react';
 // import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
 const Register = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [email, updateEmail] = useState('');
+    const [updatEmail, setUpdateEmail] = useState('');
+    // const [validEmail, setValidEmail] = useState(true);
+
+    // const ValidateEmail = (val) => {
+
+    //     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(val)){
+        
+    //     setValidEmail(true)
+    //     } else{
+    //     setValidEmail(false)
+       
+    //     }
+        
+    // }
+
+
+    
+
+    
+    
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -14,7 +34,9 @@ const Register = (props) => {
             headers: new Headers({
                 'Content-Type': 'application/json'
             })
-        }).then(
+        })
+        .then
+        .then(
             (response) => response.json()
         ).then((data) => {
             props.updateToken(data.sessionToken)
@@ -33,7 +55,7 @@ const Register = (props) => {
             <input type="text" 
             id='registeremail' 
             value={email} 
-            onChange={(e) => setEmail(e.target.value)} />
+            onChange={(e) => setEmail(e.target.value)}  />
             <br/>
             <label htmlFor="password">Password:</label>
             <br/>
@@ -49,7 +71,7 @@ const Register = (props) => {
             <input type="text"
             id='registeremail'
             value={email}
-            onChange={(e) => updateEmail(e.target.value)} />
+            onChange={(e) => setUpdateEmail(e.target.value)} />
         </form>
     </div>
     )
@@ -57,48 +79,3 @@ const Register = (props) => {
 }
 
 export default Register;
-// const Register = (props) => {
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         fetch("http://localhost:3003/user/register", {
-//             method: 'POST',
-//             body: JSON.stringify({user: {email: email, password: password}}),
-//             headers: new Headers({
-//                 'Content-Type': 'application/json'
-//             })
-//         }).then(
-//             (response) => response.json()
-//         ).then((data) => {
-//             props.updateToken(data.sessionToken)
-//         })
-//     }
-
-//     return(
-//         <div>
-//         <form>
-//             <h1>Register</h1>
-//             <label htmlFor="email">Email:</label>
-//             <br/>
-//             <input type="text" 
-//             id='registeremail' 
-//             value={email} 
-//             onChange={(e) => setEmail(e.target.value)} />
-//             <br/>
-//             <label htmlFor="password">Password:</label>
-//             <br/>
-//             <input type="password" 
-//             id='registerpassword' 
-//             value={password} 
-//             onChange={(e) => setPassword(e.target.value)} />
-//             <br/>
-//             <button type="submit" onClick={handleSubmit}>Submit</button>
-//         </form>
-//     </div>
-//     )
-
-// }
-
-// export default Register;
--- src/components/site/Logout/Logout.js
+++ src/components/site/Logout/Logout.js
@@ -3,37 +3,29 @@ import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
 // import DeleteIcon from '@material-ui/icons/Delete';
 import './Logout.css'
-import logoutWoodenX from '../../../assets/logoutWoodenX.png'
+// import logoutWoodenX from '../../../assets/logoutWoodenX.png'
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}))
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     margin: theme.spacing(1),
+//   },
+// }))
 
 export default function Logout(props) {
   //const logoutWoodenX =
-  const classes = useStyles()
+  // const classes = useStyles()
 
   return (
-    <div>
-      <Button
-        onClick={props.clearSession}
-        //set undefined ternary
-          variant="contained"
-          color="secondary"
-          //setSessionToken(localStorage.setItem('token', undefined));
-          //className={classes.button}
-          // startIcon={DeleteIcon }
-          //startIcon={logoutWoodenX}
-        >
+   
+      
+     <>
           Log Out
-        </Button>
-
+       
+    </>
 
         
 
-       </div>
+     
 
 
 
